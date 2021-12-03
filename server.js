c17fe69ecb03e6a589cfc535a63a42e8b0920528
Cameron Wein <cameron.wein@gmail.com>
2021-12-03 15:10:27 -0600

    first request

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.get('/api/test', (req, res) => {
+    res.send('Hello');
+})
+
 app.listen(PORT, () => {
     console.log(`API server now on port 3001.`)
 });
